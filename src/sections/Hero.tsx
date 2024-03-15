f4bf41d5b736748cef6c22791f1dc3d70556ecbd
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-03-15 22:54:13 +0200

    Styled(Files) Added hero mobile

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,15 +9,16 @@ const Hero = () => {
           <Logo width={36} height={36} />
           <Navbar />
         </div>
-        <h1 className="text-white font-rye xs:text-[36px] xs:mt-[508px] xs:w-[320px] xs:leading-[36px] xs:text-center sm:text-[64px] sm:mt-[670px] sm:w-[566px] sm:leading-[72px] lg:text-[80px] lg:mt-[155px] lg:w-[707px] lg:leading-[80px] font-normal">
+
+        <h1 className="text-white font-rye xs:text-[36px] xs:mt-[508px] xs:w-[320px] xs:leading-[36px] xs:text-center sm:text-left lg:text-left sm:text-[64px] sm:mt-[670px] sm:w-[566px] sm:leading-[72px] lg:text-[80px] lg:mt-[155px] lg:w-[707px] lg:leading-[80px] font-normal xs:mx-auto sm:mx-0 lg:mx-0">
           GOOD LUCK TATTOO STUDIO
         </h1>
-        <p className="text-white font-playfair xs:text-[16px] xs:pt-[20px] xs:w-[248px] xs:leading-[24px] xs:text-center sm:text-[24px] sm:pt-[40px] sm:w-[361px] sm:leading-[36px] lg:text-[24px] lg:pt-[37px] lg:w-[361px] lg:leading-[36px] font-normal">
+        <p className="text-white font-playfair xs:text-[16px] xs:pt-[20px] xs:w-[248px] xs:leading-[24px] xs:text-center sm:text-left lg:text-left sm:text-[24px] sm:pt-[40px] sm:w-[361px] sm:leading-[36px] lg:text-[24px] lg:pt-[37px] lg:w-[361px] lg:leading-[36px] font-normal xs:mx-auto sm:mx-0 lg:mx-0">
           &quot;Tattoos are an expression of freedom and individuality.&quot;
         </p>
         <button
           type="button"
-          className="text-black hover:text-[darkOrange] bg-[darkOrange] hover:bg-transparent border-2 hover:border-2 border-[black] hover:border-[darkOrange] text-[24px] w-[300px] rounded-[50px] py-4 flex items-center justify-center sm:mb-[150px] lg:mb-[267px] mt-[63px]"
+          className="text-black hover:text-[darkOrange] bg-[darkOrange] hover:bg-transparent border-2 hover:border-2 border-[black] hover:border-[darkOrange] text-[24px] xs:w-[343px] sm:w-[300px] lg:w-[300px] rounded-[50px] py-4 flex items-center justify-center xs:mb-[26px] sm:mb-[150px] lg:mb-[267px] mt-[63px]"
         >
           Online-booking
         </button>
