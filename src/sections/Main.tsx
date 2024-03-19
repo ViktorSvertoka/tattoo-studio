0a6e67a5e7ba2c4d326f4685e26fc1e020c6f05f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-03-19 21:46:29 +0200

    Feat(TSX) Added section team

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -1,6 +1,7 @@
 import About from './About';
 import Hero from './Hero';
 import Services from './Services';
+import Team from './Team';
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <Hero />
       <About />
       <Services />
+      <Team />
     </>
   );
 };
---
+++ src/sections/Team.tsx
@@ -0,0 +1,9 @@
+const Team = () => {
+  return (
+    <section className="">
+      <div className="container"></div>
+    </section>
+  );
+};
+
+export default Team;
