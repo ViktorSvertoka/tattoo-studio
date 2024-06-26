e45497cb4767eba0aae1b9dbc9170fd166d2ea31
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-27 00:18:14 +0300

    Feat(TSX) Added content for section choose

--- src/sections/Choose.tsx
+++ src/sections/Choose.tsx
@@ -1,7 +1,31 @@
 const Choose = () => {
   return (
     <section className="">
-      <div className="container"></div>
+      <div className="container">
+        <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] xs:mt-[24px] sm:mt-[40px] lg:mt-[74px] xs:mb-[32px] sm:mb-[40px] lg:mb-[73px] xs:text-center sm:text-center lg:text-center">
+          Why choose us
+        </h2>
+        <ul className="flex justify-center xs:flex-col sm:flex-wrap lg:flex-row gap-[24px]">
+          <li className="flex flex-col items-center text-center gap-[30px] py-[30px] w-[302px]">
+            <h4 className="text-darkOrange font-orelega text-[40px]">15K</h4>
+            <p className="text-gray font-playfair text-[16px]">Tattoo done</p>
+          </li>
+          <li className="flex flex-col items-center text-center gap-[30px] py-[30px] w-[302px]">
+            <h4 className="text-darkOrange font-orelega text-[40px]">4K</h4>
+            <p className="text-gray font-playfair text-[16px]">
+              Satisfied customers
+            </p>
+          </li>
+          <li className="flex flex-col items-center text-center gap-[30px] py-[30px] w-[302px]">
+            <h4 className="text-darkOrange font-orelega text-[40px]">17+</h4>
+            <p className="text-gray font-playfair text-[16px]">Award winning</p>
+          </li>
+          <li className="flex flex-col items-center text-center gap-[30px] py-[30px] w-[302px]">
+            <h4 className="text-darkOrange font-orelega text-[40px]">1.2K</h4>
+            <p className="text-gray font-playfair text-[16px]">Piercing done</p>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
