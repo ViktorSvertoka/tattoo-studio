2f832d1a9f91acc954941485e11d47d4b2fa004a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-28 00:13:53 +0300

    Feat(TSX) Added content for section contacts

---
+++ src/sections/Contacts.tsx
@@ -0,0 +1,20 @@
+const Contacts = () => {
+  return (
+    <section className="">
+      <div className="container">
+        <div>
+          <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] xs:mt-[24px] sm:mt-[40px] lg:mt-[74px] xs:mb-[32px] sm:mb-[40px] lg:mb-[73px] xs:text-center sm:text-center lg:text-center">
+            Online-booking
+          </h2>
+        </div>
+        <div>
+          <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] xs:mt-[24px] sm:mt-[40px] lg:mt-[74px] xs:mb-[32px] sm:mb-[40px] lg:mb-[73px] xs:text-center sm:text-center lg:text-center">
+            Contacts
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contacts;
