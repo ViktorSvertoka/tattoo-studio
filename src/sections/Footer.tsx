f481f6dbae90b1228a8d5dfbfa23ee64603e9e96
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-05 14:00:01 +0300

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { FaSquareFacebook } from 'react-icons/fa6';
 import { AiFillInstagram } from 'react-icons/ai';
 import { TbBrandYoutubeFilled } from 'react-icons/tb';
