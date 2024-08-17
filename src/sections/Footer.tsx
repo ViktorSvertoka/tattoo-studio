6400c99c8aafd768505ddd70b5c0bc9d2ab7f7a9
Viktor Svertoka <victor.svertoka@gmail.com>
2024-08-18 00:14:02 +0300

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <section className="pt-[30px] pb-[30px]">
       <div className="container flex xs:flex-col sm:flex-row lg:flex-row sm:justify-between sm:items-center lg:justify-between lg:items-center">
-        <p className="text-gray font-playfair text-[20px] xs:order-2 sm:order-1 lg:order-1">
+        <p className="text-gray font-playfair text-[20px] xs:order-2 sm:order-1 lg:order-1 xs:pt-[16px] sm:pt-[0px] lg:pt-[0px]">
           © All rights reserved 2024
         </p>
         <ul className="flex gap-[30px] xs:order-1 sm:order-2 lg:order-2">
