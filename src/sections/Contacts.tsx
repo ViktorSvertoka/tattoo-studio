7bc376b598a96f393c24bd6c4d5e3ebe9c9d9f53
Viktor Svertoka <victor.svertoka@gmail.com>
2024-08-21 19:25:29 +0300

    Update Contacts.tsx

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -40,9 +40,9 @@ const Contacts = () => {
             </button>
           </form>
         </div>
-        <div className="xs:bg-contactsMobile sm:bg-contactsTablet lg:bg-contactsDesktop bg-cover bg-center xs:w-[375px] xs:h-[660px] sm:w-[834px] sm:h-[449px] lg:w-[708px] lg:h-[633px] sm:pt-[60px] sm:pl-[100px] lg:pt-[100px] lg:pl-[110px]">
+        <div className="xs:bg-contactsMobile sm:bg-contactsTablet lg:bg-contactsDesktop bg-cover bg-center xs:w-[375px] xs:h-[660px] sm:w-[834px] sm:h-[449px] lg:w-[708px] lg:h-[633px] sm:pt-[30px] sm:pl-[60px] lg:pt-[100px] lg:pl-[110px]">
           <div className="xs:pt-[300px] xs:pl-[80px] sm:pt-[0px] sm:pl-[0px]">
-            <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] mb-[40px]">
+            <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] xs:mb-[15px] sm:mb-[25px] lg:mb-[40px]">
               Contacts
             </h2>
             <ul className="text-white font-playfair xs:text-[16px] sm:text-[20px] lg:text-[20px] flex flex-col gap-[20px]">
