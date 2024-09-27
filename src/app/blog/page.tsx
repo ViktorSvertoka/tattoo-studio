be4ae16d79a37de5a3575da6d517f0b53194b6d1
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-27 20:12:34 +0300

    Feat(TSX) Added page blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Blog() {
+  return (
+    <section className="">
+      <div className="container">
+        <div className="flex flex-col items-center justify-between sm:pt-[19px]">
+          <Link href="/" className="text-white">
+            Go Home
+          </Link>
+          <h1 className="text-white">Blog</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
 const navLinks = [
-  { id: 1, href: '/', label: 'Home' },
-  { id: 2, href: '/about', label: 'About' },
-  { id: 3, href: '/artists', label: 'Artists' },
-  { id: 4, href: '/contacts', label: 'Contacts' },
-  { id: 5, href: '/gallery', label: 'Gallery' },
+  { id: 1, href: '/about', label: 'About' },
+  { id: 2, href: '/artists', label: 'Artists' },
+  { id: 3, href: '/contacts', label: 'Contacts' },
+  { id: 4, href: '/gallery', label: 'Gallery' },
+  { id: 5, href: '/blog', label: 'Blog' },
 ];
 
 const Navbar = () => {
