800c0545f07ae5c4eb566141ec44bbac3d401b30
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-06 00:33:09 +0300

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -24,11 +24,13 @@ const footerLinks = [
 ];
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <section className="pt-[30px] pb-[30px]">
       <div className="container flex xs:items-center xs:flex-col xs:sm:flex-row lg:flex-row sm:justify-between sm:items-center lg:justify-between lg:items-center">
         <p className="text-gray font-playfair text-[20px] xs:order-2 sm:order-1 lg:order-1 xs:pt-[16px] sm:pt-[0px] lg:pt-[0px]">
-          © All rights reserved 2024
+          © All rights reserved {currentYear}
         </p>
         <ul className="flex gap-[30px] xs:order-1 sm:order-2 lg:order-2">
           {footerLinks.map(({ id, href, icon: Icon, size }) => (
