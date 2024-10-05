4ad11e8da90aa8f75d70694a41b1da1cce34d315
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-05 19:58:34 +0300

    Update TextComponent.tsx

--- src/components/ui/TextComponent.tsx
+++ src/components/ui/TextComponent.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const paragraphs = [
   'Welcome to Good Luck Tattoo Studio, where art and ink converge to create unforgettable masterpieces. Our studio is a haven for self-expression, where we believe that every tattoo tells a unique story.',
   'At Good Luck Tattoo Studio, we understand the significance of a tattoo as a lifelong commitment. That’s why we take the time to listen to your ideas, offering personalized consultations to ensure that your tattoo is not just a design but a reflection of your story, values, and aspirations.',
