26340eccc1baf234b0ae6448d8f3c51dd0673220
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-08 22:32:11 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hero from '../components/sections/Hero';
 import About from '../components/sections/About';
 import Services from '../components/sections/Services';
