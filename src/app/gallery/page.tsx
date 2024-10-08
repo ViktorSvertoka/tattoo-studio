64a7565f07e129f0b8a3edfb57dfaa3e38e068b9
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-08 22:27:23 +0300

    Feat(JPG & TSX) Added files nj section gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -8,7 +8,9 @@ export default function Gallery() {
           <Link href="/" className="text-white">
             Go Home
           </Link>
-          <h1 className="text-white">Gallery</h1>
+          <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] xs:mt-[16px] sm:mt-[60px] lg:mt-[100px] xs:mb-[16px] sm:mb-[40px] lg:mb-[80px] text-center">
+            Gallery
+          </h2>
         </div>
       </div>
     </section>
