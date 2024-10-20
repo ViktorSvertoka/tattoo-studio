e9c7b0feb4135a2f4ddb8538b1d8890dd01f5f2f
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-20 17:01:56 +0300

    Refactor(TS & TSX) Code edits without fixing bugs or adding new features

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,12 +1,5 @@
 import Link from 'next/link';
-
-const navLinks = [
-  { id: 1, href: '/about', label: 'About' },
-  { id: 2, href: '/artists', label: 'Artists' },
-  { id: 3, href: '/contacts', label: 'Contacts' },
-  { id: 4, href: '/gallery', label: 'Gallery' },
-  { id: 5, href: '/blog', label: 'Blog' },
-];
+import { navLinks } from '../../data/dataLinks';
 
 const Navbar = () => {
   return (
---
+++ src/data/dataLinks.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { id: 1, href: '/about', label: 'About' },
+  { id: 2, href: '/artists', label: 'Artists' },
+  { id: 3, href: '/contacts', label: 'Contacts' },
+  { id: 4, href: '/gallery', label: 'Gallery' },
+  { id: 5, href: '/blog', label: 'Blog' },
+];
