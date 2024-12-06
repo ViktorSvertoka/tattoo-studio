7b48cb34b6444b10ea1f3ef837b1c8972961d16a
Viktor Svertoka <victor.svertoka@gmail.com>
2024-12-06 16:19:59 +0200

    Update Modal.tsx

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,6 @@
 import { RiCloseLine } from 'react-icons/ri';
 import { useEffect } from 'react';
+import ContactForm from './ContactForm';
 
 interface ModalProps {
   isOpen: boolean;
@@ -50,60 +51,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
             <h2 className="text-white font-rye xs:text-[24px] sm:text-[36px] lg:text-[48px] xs:text-center sm:text-center lg:text-center">
               Online-booking
             </h2>
-            <form className="flex flex-col xs:w-[300px] sm:w-[400px] lg:w-[458px] m-auto mb-[60px]">
-              <label
-                htmlFor="name"
-                className="text-gray font-playfair text-[16px]"
-              >
-                Name
-              </label>
-              <input
-                id="name"
-                type="text"
-                className="text-gray rounded-[4px] border border-darkOrange bg-transparent outline-none focus:border-white h-[50px] mb-[16px] pl-[10px]"
-              />
-              <label
-                htmlFor="email"
-                className="text-gray font-playfair text-[16px]"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="text-gray rounded-[4px] border border-darkOrange bg-transparent outline-none focus:border-white h-[50px] mb-[16px] pl-[10px]"
-              />
-              <label
-                htmlFor="phone"
-                className="text-gray font-playfair text-[16px]"
-              >
-                Phone
-              </label>
-              <input
-                id="phone"
-                type="tel"
-                className="text-gray rounded-[4px] border border-darkOrange bg-transparent outline-none focus:border-white h-[50px] mb-[16px] pl-[10px]"
-              />
-              <label
-                htmlFor="message"
-                className="text-gray font-playfair text-[16px]"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                className="text-gray rounded-[4px] border border-darkOrange bg-transparent outline-none focus:border-white h-[110px] pl-[10px]"
-              ></textarea>
-              <p className="text-gray mt-[13px] xs:text-[12px]">
-                Giving your information you agree with privacy policy.
-              </p>
-              <button
-                type="submit"
-                className="hover:text-black text-darkOrange hover:bg-darkOrange bg-transparent focus:border-transparent border-2 hover:border-2 hover:border-darkOrange border-darkOrange text-[24px] xs:w-[220px] sm:w-[220px] lg:w-[220px] rounded-[50px] py-4 flex items-center justify-center mt-[45px] ml-auto mr-auto xs:mb-[32px]"
-              >
-                Submit
-              </button>
-            </form>
+            <ContactForm />
           </div>
         </div>
       </div>
