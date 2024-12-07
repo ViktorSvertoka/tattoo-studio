32ecda245661242ba6febb7f0e115514baec1574
Viktor Svertoka <victor.svertoka@gmail.com>
2024-12-07 19:39:53 +0200

    Style(CSS) Code style edits

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -14,15 +14,33 @@ export default function Gallery() {
     <>
       <section>
         <div className="container">
-          <div className="flex items-center w-full sm:pt-[19px] xs:mb-[32px] sm:mb-[40px] lg:mb-[73px]">
-            <Link href="/" className="text-white mr-auto">
-              <GoBack width={40} height={40} />
+          <div className="flex items-center w-full sm:mt-[20px] xs:mb-[20px] sm:mb-[20px] lg:mb-[20px]">
+            <Link
+              href="/"
+              className="group text-white hover:text-darkOrange flex items-center mr-auto font-rye xs:text-[14px] sm:text-[24px] lg:text-[24px] transition-colors duration-300 ease-in-out"
+            >
+              <GoBack
+                width={40}
+                height={40}
+                className="mr-4 transition-colors duration-300 ease-in-out group-hover:fill-white"
+              />
+              Go Home
             </Link>
             <h2 className="text-white font-rye xs:text-[32px] sm:text-[48px] lg:text-[48px] text-center flex-grow">
               Gallery
             </h2>
+            <Link
+              href="/blog"
+              className="group text-white hover:text-darkOrange flex items-center ml-auto font-rye xs:text-[14px] sm:text-[24px] lg:text-[24px] transition-colors duration-300 ease-in-out"
+            >
+              Go Blog
+              <GoBack
+                className="rotate-180 ml-4 transition-colors duration-300 ease-in-out group-hover:fill-white"
+                width={40}
+                height={40}
+              />
+            </Link>
           </div>
-
           <GridList />
         </div>
       </section>
