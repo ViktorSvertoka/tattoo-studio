74d8765f7f52be6a55282470853007d88492dd08
Viktor Svertoka <victor.svertoka@gmail.com>
2026-09-01 11:15:24 +0300

    Remove carousel arrow controls

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,11 +1,7 @@
 'use client';
 import Image from 'next/image';
-import {
-  HiOutlineArrowLongLeft,
-  HiOutlineArrowLongRight,
-} from 'react-icons/hi2';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Keyboard, Navigation, Pagination } from 'swiper/modules';
+import { A11y, Keyboard, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { teamMembers } from '../../data/dataTeam';
@@ -18,10 +14,6 @@ export default function Slider() {
         slidesPerView={1}
         spaceBetween={10}
         loop={true}
-        navigation={{
-          prevEl: '.artists-prev',
-          nextEl: '.artists-next',
-        }}
         keyboard={{
           enabled: true,
           onlyInViewport: true,
@@ -45,11 +37,9 @@ export default function Slider() {
             spaceBetween: 50,
           },
         }}
-        modules={[A11y, Keyboard, Navigation, Pagination]}
+        modules={[A11y, Keyboard, Pagination]}
         a11y={{
           containerMessage: 'Tattoo artists carousel',
-          nextSlideMessage: 'Show next artist',
-          prevSlideMessage: 'Show previous artist',
           paginationBulletMessage: 'Show artist group {{index}}',
         }}
         className="mySwiper h-[420px]"
@@ -77,35 +67,11 @@ export default function Slider() {
         ))}
       </Swiper>
 
-      <div className="mt-7 flex items-center justify-center gap-5 sm:gap-7">
-        <button
-          type="button"
-          aria-label="Show previous artist"
-          aria-controls="artists-carousel"
-          className="artists-prev group inline-flex h-11 min-w-11 items-center justify-center text-[#a8a8a8] transition-colors hover:text-darkOrange focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-darkOrange"
-        >
-          <HiOutlineArrowLongLeft
-            aria-hidden="true"
-            className="h-7 w-8 transition-transform group-hover:-translate-x-1"
-          />
-        </button>
-
+      <div className="mt-7 flex min-h-6 items-center justify-center">
         <div
           className="artists-pagination"
           aria-label="Choose artist group"
         />
-
-        <button
-          type="button"
-          aria-label="Show next artist"
-          aria-controls="artists-carousel"
-          className="artists-next group inline-flex h-11 min-w-11 items-center justify-center text-[#a8a8a8] transition-colors hover:text-darkOrange focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-darkOrange"
-        >
-          <HiOutlineArrowLongRight
-            aria-hidden="true"
-            className="h-7 w-8 transition-transform group-hover:translate-x-1"
-          />
-        </button>
       </div>
     </div>
   );
