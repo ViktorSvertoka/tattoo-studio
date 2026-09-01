53f023649648f21ad5752454200363632968f176
Viktor Svertoka <victor.svertoka@gmail.com>
2026-09-01 11:24:49 +0300

    Reset scroll position on route changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>{children}</body>
     </html>
   );
